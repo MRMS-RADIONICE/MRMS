7c8c4a7b5d5408bd7163ce589485db8c3c2a19be
Dino Keran <51824884+DBKeran@users.noreply.github.com>
2022-03-29 12:44:19 +0200

    Dodan osnovni program za 18. radionicu

--- src/radionica18.h
+++ src/radionica18.h
@@ -2,7 +2,48 @@
 #include <helper.h>
 
 void RobotLine::radionica18() {
+    if (setup()){
+        servo(90, 0);   // Raspon stupnjeva od 0 do 90 do 140
+        servo(40, 1);   // Raspon stupnjeva od 
+        servo(60, 2);   // Raspon stupnjeva od 
+    }
     
+    if (lineAny()){
+        lineFollow();
+    }
+    else
+    {
+        if (frontRight < 200 and frontLeft > 200)
+        {
+            rightWall();  
+        }
+        else
+            leftWall();
+    }
+}
+
+void rightWall(){
+    if (front() < 100){
+        go(70, -70);
+        delayMs(600);
+    }
+    if (frontRight < 150){
+        go(-70, 70);   
+    }
+    else
+        go(70, -70);
+}
+
+void leftWall(){
+    if (front() < 100){
+        go(-70, 70);
+        delayMs(600);
+    }
+    if (frontLeft < 150){
+        go(70, -70);
+    }
+    else
+        go(-70, 70);
 }
 
 /*
