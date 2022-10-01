ff0c0c635ba8a14029e3232558459bd5e6480fbb
MRMS-RADIONICE <102509280+MRMS-RADIONICE@users.noreply.github.com>
2022-10-01 09:47:29 +0200

    Update 1-radionica.h

--- src/radionice-spasavanja/1-radionica.h
+++ src/radionice-spasavanja/1-radionica.h
@@ -1,1 +1,25 @@
+#pragma once
+#include "helper.h"
+void RobotLine::radionica() {
+// ZADATAK: Prikaži naziv robota.
+// HINT: display(); 
+  display("SIMBA");
 
+// ZADATAK: Neka robot opiše četverokut. 
+  go(90, 90);
+  delayMs(2000);
+  go(-90, 90);
+  delayMs(500);
+  go(90, 90);
+  delayMs(2000);
+  go(-90, 90);
+  delayMs(500);
+  go(90, 90);
+  delayMs(2000);
+  go(-90, 90);
+  delayMs(500);  
+  go(90, 90);
+  delayMs(2000);
+  stop();
+  end();
+}
